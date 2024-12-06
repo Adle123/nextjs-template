96d812f8b4ce8aa0f256153678292ad8e697ec8f
Adle123 <adelineclark2023@gmail.com>
2024-12-06 13:42:08 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-'use client';
-import {useEffect} from 'react';
+'use client'; // This marks the file as a Client Component.
 
-import Image from 'next/image'
+import { useEffect } from 'react';
+import Image from 'next/image';
 
 export default function Home() {
-useEffect(()=>{
-  window.location.hre = 'https://gold888rtp.pics/'; }, []);
+  useEffect(() => {
+    // Redirecting to the target URL
+    window.location.href = 'https://gold888rtp.top';
+  }, []); // Empty dependency array ensures this runs only on mount
+
   return (
     <main className="w-3/4 flex flex-col items-center gap-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 absolute">
       <div className="flex items-center gap-[50px]">
@@ -36,33 +39,30 @@ useEffect(()=>{
         This is a template for creating a Next.js site and deploying it on Fleek.
       </p>
 
-      <ul
-        className='grid grid-cols-2 gap-[15px]'
-        role='list'
-      >
+      <ul className="grid grid-cols-2 gap-[15px]" role="list">
         <Card
-          icon='/fleekMark.svg'
+          icon="/fleekMark.svg"
           width={19}
           href="https://docs.fleek.xyz/"
           title="Fleek Documentation"
-          body="Learn about Fleek & the available services by cheking our official docs."
+          body="Learn about Fleek & the available services by checking our official docs."
         />
         <Card
-          icon='/nextMark.svg'
+          icon="/nextMark.svg"
           width={31}
           href="https://nextjs.org/docs"
           title="Next.js Documentation"
           body="Learn about how Next.js works and explore the official API docs."
         />
         <Card
-          icon='/fleekMark.svg'
+          icon="/fleekMark.svg"
           width={19}
           href="https://blog.fleek.xyz/"
           title="Fleek Blog"
           body="Checkout our Blog for more information about Fleek."
         />
         <Card
-          icon='/nextMark.svg'
+          icon="/nextMark.svg"
           width={31}
           href="https://astro.build/integrations/"
           title="Next.js Blog"
@@ -70,7 +70,7 @@ useEffect(()=>{
         />
       </ul>
     </main>
-  )
+  );
 }
 
 type CardProps = {
@@ -79,27 +79,18 @@ type CardProps = {
   href: string;
   icon: string;
   width: number;
-}
+};
 
 const Card = ({ title, width, body, href, icon }: CardProps) => {
   return (
     <li className="list-none">
       <a href={href} className="h-full flex flex-col no-underline rounded-3xl border-[0.85px] border-[#313538] gap-[15px] px-8 py-5">
-        <div className="flex gap-[15px] items-start ">
-          <Image
-            alt="card-icon"
-            src={icon}
-            height={31}
-            width={width}
-          />
-          <h2 className="text-xl font-bold leading-8 m-0 text-white">
-            {title}
-          </h2>
+        <div className="flex gap-[15px] items-start">
+          <Image alt="card-icon" src={icon} height={31} width={width} />
+          <h2 className="text-xl font-bold leading-8 m-0 text-white">{title}</h2>
         </div>
-        <p className="text-md leading-6 text-[#9ca3af]">
-          {body}
-        </p>
+        <p className="text-md leading-6 text-[#9ca3af]">{body}</p>
       </a>
     </li>
-  )
-}
+  );
+};
