deca40c46c08b4931b403f8ba1fc8a53d993628c
Adle123 <adelineclark2023@gmail.com>
2024-12-06 14:30:13 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
- 
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://gold888rtp.pics/:path*', // this is where content is being served
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig;
